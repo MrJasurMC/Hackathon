73a5a60633cfe4945725dcd98c6ad14642099e72
Muhammadsoli <you@example.com>
2026-04-02 18:04:04 +0500

    men transti qildim

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
 import Header from "./components/Header/Header.jsx"
 import Sidebar from "./components/Sidebar/Sidebar.jsx"
-<<<<<<< HEAD
 import Transactions from "./pages/Transactions/Transactions.jsx"
-=======
 import Financial from "./pages/Financial/Financial.jsx"
-import Transactions from "./pages/Transactions/Transactions.jsx"
 import Users from "./pages/Users/Users.jsx"
->>>>>>> 563f046876bd0fc25e49526a9c21070af366f0e8
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -18,13 +14,13 @@ function App() {
       <main>
         <Header />
         <Routes>
-<<<<<<< HEAD
+
           <Route path='/transactions' element={<Transactions />} />
-=======
+
           <Route path='/' element={<Financial />} />
           <Route path='/' element={<Transactions />} />
           <Route path='/' element={<Users />} />
->>>>>>> 563f046876bd0fc25e49526a9c21070af366f0e8
+
         </Routes>
       </main>
     </section>
