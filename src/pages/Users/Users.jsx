e6c840d36bd0b49f8dfd693c5cc81a2cff910417
Jasurbek <mrjasurmc@gmail.com>
2026-04-02 17:22:07 +0500

    changed

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { LuUserRoundSearch } from "react-icons/lu";
 
 function Users() {
   return (
-    <section>
-      
+    <section className='users'>
+      <div className="users-header">
+        <div className="users-header-text">
+          <p>ADMIN / <span>User Directory</span></p>
+          <h1>User Directory</h1>
+          <p>Curate your organization's access hierarchy. Manage roles, permissions, and active
+            status for all digital curators within the Fiscal Atelier ecosystem.</p>
+        </div>
+        <div className="add-user">
+
+        </div>
+
+      </div>
+      <div className="user-email">
+        <LuUserRoundSearch />
+        <div className="user-input">
+          <input type="text" />
+        </div>
+      </div>
     </section>
-      )
+  )
 }
 
 export default Users
