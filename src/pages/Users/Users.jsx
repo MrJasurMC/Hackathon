1351fbe0d7f56bd978488ad457417ff0df1198bd
Jasurbek <mrjasurmc@gmail.com>
2026-04-02 17:38:20 +0500

    changed

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
 import { LuUserRoundSearch } from "react-icons/lu";
+import { RiShieldUserLine } from "react-icons/ri";
+import "./Users.css"
 
 function Users() {
   return (
     <section className='users'>
       <div className="users-header">
+        <div className="user-slash">
+          <p>ADMIN / <span>USER DIRECTORY</span></p>
+        </div>
         <div className="users-header-text">
-          <p>ADMIN / <span>User Directory</span></p>
-          <h1>User Directory</h1>
-          <p>Curate your organization's access hierarchy. Manage roles, permissions, and active
-            status for all digital curators within the Fiscal Atelier ecosystem.</p>
+          <div className="user-text">
+            <h1>User Management</h1>
+            <p>Curate your organization's access hierarchy. Manage roles, permissions, and active
+              status for all digital curators within the Fiscal Atelier ecosystem.</p>
+          </div>
+          <div className="add-user">
+            + Add User
+          </div>
         </div>
-        <div className="add-user">
-
-        </div>
-
       </div>
+
       <div className="user-email">
         <LuUserRoundSearch />
         <div className="user-input">
           <input type="text" />
+        </div>
+        <div className="roles">
+          <RiShieldUserLine />
+          <select>
+            <option value="All Roles"></option>
+          </select>
+          <div className="actives">
+            <div className="active">
+              <span>Total Curators</span>
+              <h3>142</h3>
+            </div>
+            <div className="active activated">
+              <span>Active Now</span>
+              <h3>12</h3>
+            </div>
+          </div>
         </div>
       </div>
     </section>
