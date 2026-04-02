e1761c7e776f35eec9f3805c052ae7c33aada675
Jasurbek <mrjasurmc@gmail.com>
2026-04-02 18:03:10 +0500

    changed

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -23,15 +23,16 @@ function Users() {
       </div>
 
       <div className="user-email">
-        <LuUserRoundSearch />
         <div className="user-input">
-          <input type="text" />
+        <LuUserRoundSearch />
+          <input type="text" placeholder='Find user by name or email...'/>
         </div>
         <div className="roles">
           <RiShieldUserLine />
           <select>
-            <option value="All Roles"></option>
+            <option value="All Roles">All Roles</option>
           </select>
+          </div>
           <div className="actives">
             <div className="active">
               <span>Total Curators</span>
@@ -42,8 +43,22 @@ function Users() {
               <h3>12</h3>
             </div>
           </div>
+      </div>
+      <div className="currators">
+        <div className="currators-header">
+          <h3>Curator Name</h3>
+          <div className="currators-header-buffs">
+            <h3>Access Role</h3>
+            <h3>Access Role</h3>
+            <h3>Current Status</h3>
+          </div>
+          <h3>Actions</h3>
+        </div>
+        <div className="currators-main">
+          
         </div>
       </div>
+
     </section>
   )
 }
